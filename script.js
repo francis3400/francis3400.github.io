16ebdffab0bcac530f7625254ce6b8673259c6f6
Amen TETTEH <tkamen@outlook.com>
2022-10-21 17:24:48 +0000

    Update script.js to load dynamically projects

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const mobileLinks = document.querySelectorAll('#mobile-nav');
 const modal = document.querySelector('.modal');
 const modalInner = document.querySelector('.modal-inner');
 const cancelsModal = document.querySelector('.cancels');
-const works = document.querySelector('#Portfolio');
+const works = document.querySelector('#cards');
+
+
 
 hamburger.addEventListener('click', () => {
   mobileDisplay.classList.add('show');
@@ -28,13 +30,14 @@ mobileLinks.forEach((item) => {
 const projects = [
   {
     id: 0,
-    class: 'tonic',
     name: {
       mobile: 'Tonic',
       desktop: 'Tonic',
     },
-    stack: ['CANOPY', 'Back End Dev', 2015],
-    mobileStack: ['CANOPY', 'Back End Dev', 2015],
+    stack: {
+      mobile:  ['CANOPY', 'Back End Dev', 2015],
+      desktop: ['FACEBOOK', 'Full Stack Dev', 2015],
+    },
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
     image: {
       mobile: 'images/snap10.png',
@@ -46,16 +49,18 @@ const projects = [
     },
     linkToLiveVersion: 'https://francis3400.github.io/',
     linkToSource: 'https://github.com/francis3400/francis3400.github.io/',
+    imageAtTheLeft:false
   },
   {
     id: 1,
-    class: 'stories',
     name: {
       mobile: 'Multi-Post Stories',
       desktop: 'Multi-Post Stories',
     },
-    stack: ['FACEBOOK', 'Full Stack Dev', 2015],
-    mobileStack: ['CANOPY', 'Back End Dev', 2015],
+    stack: {
+      mobile:  ['CANOPY', 'Back End Dev', 2015],
+      desktop: ['FACEBOOK', 'Full Stack Dev', 2015],
+    },
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
     image: {
       mobile: 'images/snapshot2.png',
@@ -67,16 +72,18 @@ const projects = [
     },
     linkToLiveVersion: 'https://francis3400.github.io/',
     linkToSource: 'https://github.com/francis3400/francis3400.github.io/',
+    imageAtTheLeft:true
   },
   {
     id: 2,
-    class: 'tonic2',
     name: {
       mobile: 'Tonic',
       desktop: 'Facebook 360',
     },
-    stack: ['FACEBOOK', 'Full Stack Dev', 2015],
-    mobileStack: ['CANOPY', 'Back End Dev', 2015],
+    stack: {
+      mobile:  ['CANOPY', 'Back End Dev', 2015],
+      desktop: ['FACEBOOK', 'Full Stack Dev', 2015],
+    },
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
     image: {
       mobile: 'images/snapshoot3.png',
@@ -88,16 +95,18 @@ const projects = [
     },
     linkToLiveVersion: 'https://francis3400.github.io/',
     linkToSource: 'https://github.com/francis3400/francis3400.github.io/',
+    imageAtTheLeft:false
   },
   {
     id: 3,
-    class: 'stories2',
     name: {
       mobile: 'Multi-Post Stories',
       desktop: 'Uber Navigation',
     },
-    stack: ['Uber', 'Lead Developer', 2018],
-    mobileStack: ['CANOPY', 'Back End Dev', 2015],
+    stack: {
+      mobile:  ['CANOPY', 'Back End Dev', 2015],
+      desktop: ['Uber', 'Lead Developer', 2018],
+    },
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
     image: {
       mobile: 'images/snapshot4.png',
@@ -109,41 +118,74 @@ const projects = [
     },
     linkToLiveVersion: 'https://francis3400.github.io/',
     linkToSource: 'https://github.com/francis3400/francis3400.github.io/',
+    imageAtTheLeft:true
   },
 ];
 
-const openModal = ({
-  name,
-  description,
-  image,
-  technologies,
-  stack,
-  mobileStack,
-  linkToLiveVersion,
-  linkToSource,
-}) => {
+buildProjectSession(projects)
+
+openModal()
+
+function openModal(
+  id=0,
+  name= {
+    mobile: 'Multi-Post Stories',
+    desktop: 'Uber Navigation',
+  },
+  stack={
+    mobile:  ['CANOPY', 'Back End Dev', 2015],
+    desktop: ['Uber', 'Lead Developer', 2018],
+  },
+  description="qqqqqqqqqqqqq",
+  image=  {
+    mobile: 'images/snap10.png',
+    desktop: 'images/Snapshoot_Portfolio.png',
+  },
+  technologies={
+    mobile: ['html', 'css', 'javascript'],
+    desktop: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+  },
+  linkToLiveVersion= 'https://francis3400.github.io/',
+  linkToSource= 'https://github.com/francis3400/francis3400.github.io/',
+)  {
   let display = '';
-  modal.classList.add('showModal');
+
+
+  //modal.classList.add('showModal');
   display += `
   <div>
-
-  <div>
-  <h2 class="main-text">${name}</h2>
+  <h2 class="main-text">${isMobile()? name.mobile:name.desktop}</h2>
   </div>
 
-  <ul class="tonicListQ">
-  <li class="licap sub-list">CANOPY</li>
-  <li class="sub-list">• Back End Dev</li>
-  <li class="sub-list">• 2015</li>
-  </ul>
-    <ul class="tonicListR bax">
-            <li class="licap sub-list">CANOPY</li>
-            <li class="sub-list">• Back End Dev</li>
-            <li class="sub-list">• 2015</li>
+  <ul class="tonicListQ">`
+  console.log("****************************************************")
+  console.log(display)
+  let stacks=""
+  let stackArray=stack.mobile
+  for (let i = 0; i < stackArray.length; i++) {
+    stacks+=`<li class="licap sub-list">${stackArray[i]}</li>`
+  }
+  display+=stacks
+  display+=
+  `</ul>
+  <ul class="tonicListR bax">`
+
+  stacks=""
+  stackArray=stack.desktop
+  for (let i = 0; i < stackArray.length; i++) {
+    if(i=== 0){
+      stacks+=`<li class="licap sub-list">${stackArray[i]}</li>`
+    }else{
+      stacks+=`<li class="sub-list">${stackArray[i]}</li>`
+    }
+
+  } 
+  display+=stacks
+
+  display+=`
             </ul>
-  <div>
-  
-    <img class="pic-work" src=${image} alt="Project"/>
+  <div>  
+    <img class="pic-work" src=${isMobile()? image.mobile:image.desktop} alt="Project"/>
   </div>
   <div class="blab">
    <div> <p class="main-p">
@@ -152,6 +194,9 @@ const openModal = ({
     </div>
     <div class="hidden-buttons">
     <ul class="htmlButtons button">`;
+  
+    console.log("****************************************************")
+    console.log(display)   
 
   let techs = '';
   for (let i = 0; i < technologies.length; i++) {
@@ -169,9 +214,19 @@ const openModal = ({
     </div>
     </div>
 </div>`;
-  modalInner.innerHTML = display;
+  modal.innerHTML = display;
 };
 
+
+document.querySelectorAll(".seeProject .btn-modal").forEach((item) =>
+    item.addEventListener("click", () => {
+        console.log("xxxxxxxxxxxxxxxxxxx");
+        console.log("xxxxxxxxxxxxxxxxxxx");
+        modal.classList.add('showModal');
+
+
+    })
+);
 
 
 const loadModal = (i) => {
@@ -179,107 +234,95 @@ const loadModal = (i) => {
   openModal(singleData);
 };
 
-cancelsModal.addEventListener('click', () => {
-  modal.classList.remove('showModal');
-});
+// cancelsModal.addEventListener('click', () => {
+//   modal.classList.remove('showModal');
+// });
 
-const loadWorks = (data) => {
-  let display = ' ';
-  data.forEach(
-    (
-      {
-        name,
-        mainDescription,
-        featuredImage,
-        desktopFeaturedImage,
-        technologies,
-        stack,
-        mobileStack,
-      },
-      i
-    ) => {
-      display += `
-      <div class="card-works works">
-      <div class="image ">
-        <img class="desktop-image" src= ${
-          isDesktop ? desktopFeaturedImage : featuredImage
-        } alt="Project"/>
-      </div>
-      <div class="card">
-        <h2>${name}</h2>
-        <ul class="flex item-tag">
-          ${
-            isDesktop
-              ? `<li class="icon-tag"><a class="canopy" href="#">${stack[0]}</a></li>`
-              : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[0]}</a></li>`
-          }
-          <li class="radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-          ${
-            isDesktop
-              ? `<li class="icon-tag"><a class="bend-yr bend" href="#">${stack[1]}</a></li>`
-              : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[1]}</a></li>`
-          }
-          <li class=" radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-          ${
-            isDesktop
-              ? `<li class="icon-tag"><a class="bend-yr"href="#">${stack[2]}</a></li>`
-              : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[2]}</a></li>`
-          }
-        </ul>
-        <p class="p">
-          ${mainDescription}
-        </p>
-        <div class="langbutton">
-          <button>${technologies[0]}</button>
-          <button>${technologies[4]}</button>
-          <button>${technologies[1]}</button>
-          ${isDesktop ? `<button>${technologies[2]}</button>` : ''}
-        </div>
+
+function buildProjectSession(data){
+  let display = '';
   
-        <div class="div">
-        <button type="submit" id="loadModal" value="${i}" onclick='loadModal(${i}) '  class="btn-modal btn">See Project</button>
+  data.forEach(
+    (item) => {
+         display+=` <section class="parting">
+      <div class="card-works">`
+      if(item.imageAtTheLeft){
+        display+=`<div class="tonic-pic">`
+      }else{
+        display+=`<div class="tonic-pic text-first">`
+      }
+      display+=`
+        <div class="tonic-pic">
+          <img class="photo1" src=${item.image.mobile} />
+          <img class="photo2" src=${item.image.desktop} />
         </div>
+        <div class="tonic-text">
+          <h2 class="main-text">${isMobile()? item.name.mobile:item.name.desktop}</h2>
+          <ul class="tonicListQ">`;
+          let stacks=""
+          let stackArray=item.stack.mobile
+          for (let i = 0; i < stackArray.length; i++) {
+              if(i===0){
+                stacks+=`<li class="licap sub-list">${stackArray[i]}</li>`
+              }else{
+                stacks+=`<li class="sub-list">${stackArray[i]}</li>`
+              }
+          }
+          display+=stacks
+          display+=
+          `</ul>
+          <ul class="tonicListR">`
+
+          stacks=""
+          stackArray=item.stack.desktop
+          for (let i = 0; i < stackArray.length; i++) {
+            if(i===0){
+              stacks+=`<li class="licap sub-list">${stackArray[i]}</li>`
+            }else{
+              stacks+=`<li class="sub-list">${stackArray[i]}</li>`
+            }
+           
+          }  
+          display+=stacks
+
+          display+=`
+          </ul>
+          <p class="main-p">
+          ${item.description}
+          </p>
+          <ul class="htmlButtons">`
+          let tech=""
+          techsArray=isMobile()? item.technologies.mobile:item.technologies.desktop
+          for (let i = 0; i < techsArray.length; i++) {
+            tech+=`<li><button type="button">${techsArray[i]}</button></li>`
+          }  
+          display+=tech
+
+          display+=`
+          </ul>
+          <div class="seeProject">
+            <button id="seeMultiPost" class="btn-modal btn" type="submit">See Project</button>
+          </div>
         </div>
-      </div> `;
+      </div>
+    </section>`
     }
   );
+
+  console.log(display)
+
+/*   document
+  .getElementById("cards")
+  .insertAdjacentHTML("afterbegin",display); */
+
   works.innerHTML = display;
+
 };
 
-window.addEventListener('DOMContentLoaded', () => {
-  loadWorks(data);
-
-  const loadModalEle = document.querySelector('#loadModal');
-  loadModalEle.addEventListener('click', () => {
-    loadModal(loadModalEle.value);
-  });
-});
-
-modalBtn.forEach((btn, i) => {
-  console.log(btn, 'WHAT', i);
-
-  btn.addEventListener('click', () => {
-    const singleData = projects.find((item) => item.id == i);
-    singleData.technologies = isMobile()
-      ? singleData.technologies['mobile']
-      : singleData.technologies['desktop'];
-    singleData.image = isMobile()
-      ? singleData.image['mobile']
-      : singleData.image['desktop'];
-    singleData.name = isMobile()
-      ? singleData.name['mobile']
-      : singleData.name['desktop'];
-    openModal(singleData);
-    console.log('none');
-    console.log(window.innerWidth);
-    console.log(singleData.technologies);
-  });
-});
-
-cancelsModal.addEventListener('click', () => {
-  modal.classList.remove('showModal');
-});
 
 function isMobile() {
   return window.innerWidth <= 768;
 }
+
+
+
