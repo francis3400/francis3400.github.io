e3434fa3addb41e57b5acad9e0eb80f17de851d7
Amen TETTEH <tkamen@outlook.com>
2022-10-22 11:37:00 +0000

    Update script.js to add form validation

--- script.js
+++ script.js
@@ -323,6 +323,27 @@ function buildProjectSession(data){
 function isMobile() {
   return window.innerWidth <= 768;
 }
+//Form validation 
 
+let form = document.querySelector('#contact-style form'),
+  errMessageZone = document.querySelector('#error-message'),
+  emailClass = document.querySelector('#contact-style .email');
 
+form.addEventListener('submit', (event) => {
+  console.log("11FHFFFFFFFFFFFFFFF1")
+  let email = document.querySelector('#email');
+  let emailRegExp = /[A-Z]/;
+  console.log("FHFFFFFFFFFFFFFFF1")
+  console.log(email.value)
+  console.log(email.value.trim())
+  if (!emailRegExp.test(email.value.trim())) {
+    console.log("FHFFFFFFFFFFFFFFF2")
+    form.submit();
+  } else {
+    event.preventDefault();
+    //emailClass.classList.toggle('error');
+    console.log("FHFFFFFFFFFFFFFFF")
+    errMessageZone.innerText ="Lower Case Email Please"
+  }
+});
 
